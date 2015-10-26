e4742db8831b7e77e0e947b0046b81f76b220f8a
limjc <lila44@naver.com>
2015-10-26 22:35:30 +0900

    public 추가

--- app.js
+++ app.js
@@ -3,24 +3,21 @@
     date        : 2015.10.26 22:15
     description : node.js 최초 테스트 모듈
 */
-
-var express     = require("express");
+var express     = require('express');
+var path        = require('path');
 var application = express();
 
-/*
-    인덱스 페이지
-    @param request   요청
-    @param response  응답
-    @return          환영 메세지
-*/
-application.get('/', function(request, response){
-    response.send("hello node.js 1");
-});
+
+// web path 설정
+application.use(express.static(path.join(__dirname, 'public')));
+
+// 서버 설정 확인
+console.log('web path : ' + __dirname);
 
 /*
     사용자 요청 연결
     @return  환영 메세지
 */
 application.listen(7777, function(){
-    console.log("connect..");
+    console.log('connect..');
 });
