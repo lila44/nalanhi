b7028c9a76f64e4f71fc2a596c1b3118856e1294
limjc <lila44@naver.com>
2015-10-26 22:12:58 +0900

    app.js 수정

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var application = express();
 
 /*
     인덱스 페이지
-    @param request  : 요청
-    @param response : 응답
-    @return         : 환영 메세지
+    @param request   요청
+    @param response  응답
+    @return          환영 메세지
 */
 application.get('/', function(request, response){
     response.send("hello node.js 1");
@@ -19,9 +19,7 @@ application.get('/', function(request, response){
 
 /*
     사용자 요청 연결
-    @param request  : 요청
-    @param response : 응답
-    @return         : 환영 메세지
+    @return  환영 메세지
 */
 application.listen(7777, function(){
     console.log("connect..");
