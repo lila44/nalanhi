1ccd4bd9cf90aa4d7a92f96c2c9a2ac587d022bf
limjc <lila44@naver.com>
2015-10-27 17:04:08 +0900

    restful crud

--- app.js
+++ app.js
@@ -2,11 +2,29 @@
     author      : 임정채
     date        : 2015.10.26 22:15
     description : node.js 최초 테스트 모듈
+
+    Restful
+    - Index  | GET    | 복수조회
+    - Show   | GET    | 단일조회
+    - New    | GET    | 등록폼
+    - Create | POST   | 등록저장
+    - Edit   | GET    | 수정폼
+    - Update | PUT    | 수정저장
+    - Detory | DELETE | 삭제
+
+    - body-parser
+    request.body   : Post로 넘어온 값이라고 보면 됩니다.
+    request.query  : Get으로 넘어온 값이라고 보면 됩니다.
+    request.params : RESTful 요청에 동적으로 변경되는 :variable에 대한 처리
+
 */
 var express     = require('express');
 var path        = require('path');
 var mongoose    = require('mongoose');
 var application = express();
+
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
 
 
 // ejs
@@ -15,9 +33,12 @@ application.set('view engine', 'ejs');
 // web path
 application.use(express.static(path.join(__dirname, '/public')));
 
-// 서버 설정 확인
-console.log('web path : ' + __dirname);
+// json parser
+application.use(bodyParser.json());
+application.use(bodyParser.urlencoded({extended:true}));
 
+// method override
+application.use(methodOverride("_method"));
 
 
 // database - connection
@@ -32,71 +53,61 @@ connection.on('error', function(e){
 
 // database - schema
 var schema = mongoose.Schema({
-    name  : String,
-    count : Number
+    title     : {type:String, required:true    },
+    body      : {type:String, required:true    },
+    createdAt : {type:Date,   default :Date.now},
+    updatedAt : Date
 });
 
 // database - datasource
-var datasource = mongoose.model('data', schema);
+var datasource = mongoose.model('post', schema);
 
 
 
-/// http://localhost:7777/
-application.get('/', function(request, response){
-    getCount(response);
-});
-
-// http://localhost:7777/reset
-application.get('/reset', function(request, response){
-    setCount(response, 0);
-});
-
-// http://localhost:7777/set/count?firstCount=11
-application.get('/set/count', function(request, response){
-    console.log('request.query.firstCount=' + request.query.firstCount);
-    if(request.query.firstCount){
-        setCount(response, request.query.firstCount);
-    }
-    else{
-        getCount(response);
-    }
-});
-
-// http://localhost:7777/set/1234
-application.get('/set/:num', function(request, response){
-    console.log('request.params.num=' + request.params.num);
-    if(request.params.num){
-        setCount(response, request.params.num);
-    }
-    else{
-        getCount(response);
-    }
-});
-
-function setCount(response, num){
-
-    datasource.findOne({name : "limjc"}, function(e, data){
-        if(e){ console.log("setCount error : " , e); }
-        data.count = num;
-        data.save(function(e){
-            if(e){ console.log("setCount-save error : " , e); }
-            response.render("view.ejs", data);
-        });
+// Index
+application.get('/posts', function(request, response){
+    datasource.find({}).sort('-createdAt').exec(function(e, posts){
+        if(e){ return response.json({success:false, message:e}); }
+        response.json({success:true, data:posts});
     });
-}
+});
 
-function getCount(response){
-
-    datasource.findOne({name : "limjc"}, function(e, data){
-        response.render("view.ejs", data);
+// Create
+application.post('/posts', function(request, response){
+    datasource.create(request.body.post, function(e, post){
+        if(e){ return response.json({success:false, message:e}); }
+        response.json({success:true, data:post});
     });
-}
+});
 
+// Show
+application.get('/posts/:id', function(request, response){
+    datasource.findById(request.params.id, function(e, post){
+        if(e){ console.log(e); return response.json({success:false, message:e}); }
+        response.json({success:true, data:post});
+    });
+});
 
-/*
-    사용자 요청 연결
-    @return  환영 메세지
-*/
+// Update
+application.put('/posts/:id', function(request, response){
+
+    request.body.post.updatedAt = Date.now();
+
+    datasource.findByIdAndUpdate(request.params.id, request.body.post, function(e, post){
+        if(e){ console.log(e); return response.json({success:false, message:e}); }
+        response.json({success:true, message:post._id + "updated"});
+    });
+});
+
+// Detory
+application.delete('/posts/:id', function(request, response){
+    datasource.findByIdAndRemove(request.params.id, function(e, post){
+        if(e){ console.log(e); return response.json({success:false, message:e}); }
+        response.json({success:true, message:post._id + "deleted"});
+    });
+});
+
+// listen request
 application.listen(7777, function(){
     console.log('server ready..');
 });
---
+++ app_20151027.js
@@ -0,0 +1,101 @@
+/*
+    author      : 임정채
+    date        : 2015.10.26 22:15
+    description : node.js 최초 테스트 모듈
+*/
+var express     = require('express');
+var path        = require('path');
+var mongoose    = require('mongoose');
+var application = express();
+
+
+// ejs
+application.set('view engine', 'ejs');
+
+// web path
+application.use(express.static(path.join(__dirname, '/public')));
+
+// 서버 설정 확인
+console.log('web path : ' + __dirname);
+
+
+
+// database - connection
+mongoose.connect(process.env.MONGO_DB);
+var connection = mongoose.connection;
+connection.once('open', function(){
+    console.log('db ready..');
+});
+connection.on('error', function(e){
+    console.log('db error : ' + e);
+});
+
+// database - schema
+var schema = mongoose.Schema({
+    name  : String,
+    count : Number
+});
+
+// database - datasource
+var datasource = mongoose.model('data', schema);
+
+
+
+/// http://localhost:7777/
+application.get('/', function(request, response){
+    getCount(response);
+});
+
+// http://localhost:7777/reset
+application.get('/reset', function(request, response){
+    setCount(response, 0);
+});
+
+// http://localhost:7777/set/count?firstCount=11
+application.get('/set/count', function(request, response){
+    console.log('request.query.firstCount=' + request.query.firstCount);
+    if(request.query.firstCount){
+        setCount(response, request.query.firstCount);
+    }
+    else{
+        getCount(response);
+    }
+});
+
+// http://localhost:7777/set/1234
+application.get('/set/:num', function(request, response){
+    console.log('request.params.num=' + request.params.num);
+    if(request.params.num){
+        setCount(response, request.params.num);
+    }
+    else{
+        getCount(response);
+    }
+});
+
+function setCount(response, num){
+
+    datasource.findOne({name : "limjc"}, function(e, data){
+        if(e){ console.log("setCount error : " , e); }
+        data.count = num;
+        data.save(function(e){
+            if(e){ console.log("setCount-save error : " , e); }
+            response.render("view.ejs", data);
+        });
+    });
+}
+
+function getCount(response){
+
+    datasource.findOne({name : "limjc"}, function(e, data){
+        response.render("view.ejs", data);
+    });
+}
+
+/*
+    사용자 요청 연결
+    @return  환영 메세지
+*/
+application.listen(7777, function(){
+    console.log('server ready..');
+});
